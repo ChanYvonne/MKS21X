d8017ffcc02a716f6f9b08c3f7babb1681798634
Yvonne Chan <ychan1352@gmail.com>
2016-01-04 21:20:44 -0500

    Tested times on int[10000], in order of increasing times: Arrays.Sort, selectionSort, insertionSort

--- hwSorts/Driver.java
+++ hwSorts/Driver.java
@@ -1,14 +1,23 @@
+import java.util.*;
+import java.io.*;
 public class Driver{
     public static void main(String[]args){
-	int[] cats = {1,9,2,7,3,10,8,5};
-	Sorts.printArray(cats);
-	//Sorts.insertionSort(cats);
-	Sorts.selectionSort(cats);
-	Sorts.printArray(cats);	
+	Random rangen = new Random(13524);
+	int[] test = new int[10000];
+	for (int x = 0; x < test.length; x ++){
+	    test[x] = rangen.nextInt(10000);
+	}
+	//Sorts.printArray(test);
+	Sorts.insertionSort(test);
+	Sorts.selectionSort(test);
+	Arrays.sort(test);
+	//Sorts.printArray(test);
+	/*
 	int[] dogs = {10,9,8,7,6,5,4,3,2,1,0};
 	Sorts.printArray(dogs);
 	//Sorts.insertionSort(dogs);
 	Sorts.selectionSort(dogs);
 	Sorts.printArray(dogs);	
+	*/
     }
 }
